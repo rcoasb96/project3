7d5aa0e50ceab1207480aea9e636096f5c1dd27f
Rosa Cabrera <rcabrer1@hawk.iit.edu>
2016-12-04 16:09:55 -0600

    Changed the js function to send bmi request

--- js/site.js
+++ js/site.js
@@ -1,14 +1,20 @@
 // TODO: Use a function closure and release global $
-$(document).ready(function() {
-  $('#bmi-form').on('submit', function(event) {
-    var query = $('#bmi-weight', '#bmi-height'").val();
-    $.get(
-      'https://bmi.p.mashape.com/' + query,
-      function(data) {
-        $('#bmi').append(
-           xhr.setRequestHeader("X-Mashape-Authorization", "WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo");
-        );
-      });
-    event.preventDefault();
+$.noConflict();
+(function($){
+  $(document).ready(function() {
+    $('#bmi-form').on('submit', function(event) {
+      //saving inputs
+      var weight = $('#bmi-weight').val();
+      var height = $('#bmi-height').val();
+      
+      $.get(
+          'https://bmi.p.mashape.com/WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo' + weight + height,
+          function(data){
+            $('#bmi').append(
+            '<p>Your BMI is:</p>)'+ data.bmi
+          )
+      });     
+    });
+  event.preventDefault();
   });
 });
