02571e413b78598e74833d39b976919ea0da9d96
Rosa Cabrera <rcabrer1@hawk.iit.edu>
2016-12-07 23:38:33 -0600

    Added code for submit button

--- js/site.js
+++ js/site.js
@@ -1,19 +1,21 @@
-$.ajax(
-    {
+$.noConflict();
+(function($){
+  $(document).ready(function() {
+    $('#bmi-form').on('submit', function(event) {
+      $.ajax(
+       {
         method: 'POST',
         url: "https://bmi.p.mashape.com/",
         headers: {
-            "X-Mashape-Key": "14EnqO5furmshplJ1g93Wydl6c7fp1unkUvjsnbVpbghxX9Nfb",
+            "X-Mashape-Key": "WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo",
             "Content-Type": "application/json",
             "Accept": "application/json"
         },
-        data: '{"weight":{"value":"85.00","unit":"kg"},"height":{"value":"170","unit":"cm"},"sex":"m","age":"24","waist":"34.00","hip":"40.00"}'
+      data: '{"weight":{"value":"85.00","unit":"kg"},"height":{"value":"170","unit":"cm"},"sex":"m","age":"24","waist":"34.00","hip":"40.00"}'
     }
-).done(function(data,json){
-  $('#bmi').append('<p>Your BMI is: '+data.bmi.value+'</p>');
-    $('#bmi').append('<p>Your status is: '+data.bmi.status+'</p>');
-    $('#bmi').append('<p>Your risk is: '+data.bmi.risk+'</p>');
-            
-
-    console.log("Done");
+    ).done(function(data,json){
+      $('#bmi').append('<p>Your BMI is: '+data.bmi.value+'</p>');
+      $('#bmi').append('<p>Your status is: '+data.bmi.status+'</p>');
+      $('#bmi').append('<p>Your risk is: '+data.bmi.risk+'</p>');
+      console.log("Done");
 });
