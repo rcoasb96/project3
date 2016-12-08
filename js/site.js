9f61b2fb4aaaef0e6e0fa5fc0862baa0247d4de9
Marko Trajkovikj <Marko Trajkovikj>
2016-12-07 21:59:33 -0700

    Totally changed the code, there's some progress I think...

--- js/site.js
+++ js/site.js
@@ -1,38 +1,19 @@
-// TODO: Use a function closure and release global $
-$.noConflict();
-(function($){
-  $(document).ready(function() {
-    $('#bmi-form').on('submit', function(event) {
-      //saving inputs
-      var weight = $('#bmi-weight').val();
-      var height = $('#bmi-height').val();
-      
-      var url= 'https://bmi.p.mashape.com/WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo' +weight+height;
-          
-      $.get(url).done(
-   
-        
-          function(data,json){
+$.ajax(
+    {
+        method: 'POST',
+        url: "https://bmi.p.mashape.com/",
+        headers: {
+            "X-Mashape-Key": "14EnqO5furmshplJ1g93Wydl6c7fp1unkUvjsnbVpbghxX9Nfb",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        data: '{"weight":{"value":"85.00","unit":"kg"},"height":{"value":"170","unit":"cm"},"sex":"m","age":"24","waist":"34.00","hip":"40.00"}'
+    }
+).done(function(data,json){
+  $('#bmi').append('<p>Your BMI is: '+data.bmi.value+'</p>');
+    $('#bmi').append('<p>Your status is: '+data.bmi.status+'</p>');
+    $('#bmi').append('<p>Your risk is: '+data.bmi.risk+'</p>');
             
-            
-          $('#bmi').append(
-            var v=data.bmi.value;
-            '<p>Your BMI is: '+v+'</p>'
-          );
-          $('#bmi').append(
-            var s=data.bmi.status;
-            '<p>Your status is: '+s+'</p>'
-          );
-          $('#bmi').append(
-            var r=data.bmi.risk;
-           '<p>Your risk is: '+r+'</p>'
-          );
-          
-          var string= 'Your Bmi:' +v+ '. Your status:' +s+ '. Your risk:' +r+ '.';
-        $('#results').text(string);
-      });  
-       
-      });
-  event.preventDefault();
-  });
-});(jQuery);
+
+    console.log("Done");
+});  
