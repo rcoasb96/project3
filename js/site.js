9ff8a4035f390edc908c0ccaaa44818d85cf3278
Sarina-Stoker <sstoker1@hawk.iit.edu>
2016-12-07 20:22:45 -0600

    Added back original code with append functions.

--- js/site.js
+++ js/site.js
@@ -7,24 +7,29 @@ $.noConflict();
       var weight = $('#bmi-weight').val();
       var height = $('#bmi-height').val();
       
-      var url= 'https://bmi.p.mashape.com/WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo' +weight+height;
+      var url= 'https://bmi.p.mashape.com/WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo' +weight + height;
           
       $.get(url).done(
    
         
           function(data,json){
             
-            
-          
+           $('#bmi').append(
             var v=data.bmi.value;
-     
+            '<p>Your BMI is: '+v+'</p>'
+          );
+          $('#bmi').append(
             var s=data.bmi.status;
-            
+            '<p>Your status is: '+s+'</p>'
+          );
+          $('#bmi').append(
             var r=data.bmi.risk;
-      
+           '<p>Your risk is: '+r+'</p>'
+          );
           
           var output= 'Your Bmi:' +v+ '. Your status:' +s+ '. Your risk:' +r+ '.';
           $('#results').text(output);
+      
       });  
        
     });
